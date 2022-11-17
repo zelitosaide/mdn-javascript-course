5b7a0a59679490b57c6c9285e40f582456392562
zelitosaide <zelitosaide83@gmail.com>
2022-11-17 12:11:08 +0200

    call

--- __02_call/call.js
+++ __02_call/call.js
@@ -37,3 +37,22 @@ label02: {
     assignPrintMethod.call(animails[i], i);
   }
 }
+
+label03: {
+  function greet() {
+    const reply = [
+      this.animal,
+      "typically sleep between",
+      this.sleepDuration,
+    ].join(" ");
+
+    console.log(reply);
+  }
+
+  const obj = {
+    animal: "cats",
+    sleepDuration: "12 and 16 hours",
+  };
+
+  greet.call(obj); // cats typically sleep between 12 and 16 hours
+}
